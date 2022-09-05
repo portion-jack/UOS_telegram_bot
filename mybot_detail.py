9e36fda26013b876e3900e8dfbc0a8057e4d311c
portion-jack <flycast0080@gmail.com>
2022-09-05 17:11:25 +0900

    boy detail

---
+++ mybot_detail.py
@@ -0,0 +1,14 @@
+# bot 토큰 블러우기 (github publish를 위해 토큰 따로 보관)
+
+def mybot_token():
+    token = "5781653416:AAHY5lYxGeYk6jYItuZvZm3DH5TXTdIkO80"
+    return token
+
+# Use this token to access the HTTP API:
+# 5781653416:AAHY5lYxGeYk6jYItuZvZm3DH5TXTdIkO80
+#
+# my_id :
+# 5056533977
+#
+# log_site:
+# https://api.telegram.org/bot5781653416:AAHY5lYxGeYk6jYItuZvZm3DH5TXTdIkO80/getUpdates
